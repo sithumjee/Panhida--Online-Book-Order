7c84e247549c4adeef465a9a64b7e807c876062d
sithum jeevantha <sithum.21@cse.mrt.ac.lk>
2024-06-06 13:53:57 +0530

    cart finished

--- Frontend/src/Pages/Cart/Cart.jsx
+++ Frontend/src/Pages/Cart/Cart.jsx
@@ -1,7 +1,40 @@
+import { useContext } from "react";
 import "./Cart.css";
+import { StoreContext } from "../../Context/StoreCntext";
 
 const Cart = () => {
-  return <div></div>;
+  const { cartItems, food_list, removeFromCart } = useContext(StoreContext);
+
+  return (
+    <div className="cart">
+      <div className="cart-items">
+        <div className="card-items-title">
+          <p>Items</p>
+          <p>Title</p>
+          <p>Price</p>
+          <p>Quantity</p>
+          <p>Total</p>
+          <p>Remove</p>
+        </div>
+        <br />
+        <hr />
+
+        {food_list.map((item, index) => {
+          if (cartItems[index.id] > 0) {
+            return (
+              <>
+                <div className="card-items-title card-items-item">
+                  <p>{item.name}</p>
+                </div>
+                ;
+              </>
+            );
+          }
+        })}
+        <hr />
+      </div>
+    </div>
+  );
 };
 
 export default Cart;
