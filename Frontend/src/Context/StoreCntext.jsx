5d5e6dd6329a37e738e15ef0d248f0716a0a8925
sithum jeevantha <sithum.21@cse.mrt.ac.lk>
2024-06-19 12:46:01 +0530

    addtocart functions connected with backend

--- Frontend/src/Context/StoreCntext.jsx
+++ Frontend/src/Context/StoreCntext.jsx
@@ -8,15 +8,22 @@ const StoreContextProvider = (props) => {
   const [token, setToken] = useState("");
   const [food_list, setFoodList] = useState([]);
 
-  const addToCart = (itemId) => {
+  const addToCart = async (itemId) => {
     if (cartItems[itemId]) {
       setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
     } else {
       setCartItems((prev) => ({ ...prev, [itemId]: 1 }));
     }
+    if (token) {
+      await axios.post(
+        url + "/api/cart/add",
+        { itemId },
+        { headers: { token } }
+      );
+    }
   };
 
-  const removeFromCart = (itemId) => {
+  const removeFromCart = async (itemId) => {
     if (cartItems[itemId] === 1) {
       setCartItems((prev) => {
         const newCartItems = { ...prev };
@@ -25,6 +32,13 @@ const StoreContextProvider = (props) => {
       });
     } else {
       setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
+    }
+    if (token) {
+      await axios.post(
+        url + "/api/cart/remove",
+        { itemId },
+        { headers: { token } }
+      );
     }
   };
 
@@ -46,11 +60,21 @@ const StoreContextProvider = (props) => {
     setFoodList(response.data.data);
   };
 
+  const loadCartData = async (token) => {
+    const response = await axios.post(
+      url + "/api/cart/get",
+      {},
+      { headers: { token } }
+    );
+    setCartItems(response.data.cartData);
+  };
+
   useEffect(() => {
     async function loadData() {
       await fetchFoodList();
       if (localStorage.getItem("token")) {
         setToken(localStorage.getItem("token"));
+        await loadCartData(localStorage.getItem("token"));
       }
     }
     loadData();
