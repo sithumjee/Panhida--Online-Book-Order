fdbc82f324e0dabe3c706136462ea0c7a35c25a6
sithum jeevantha <sithum.21@cse.mrt.ac.lk>
2024-06-20 00:23:32 +0530

    stripe connected to website

--- Backend/server.js
+++ Backend/server.js
@@ -5,6 +5,7 @@ import foodRouter from "./routes/foodRoutes.js";
 import userRouter from "./routes/userRoute.js";
 import "dotenv/config";
 import cartRouter from "./routes/cartRoute.js";
+import orderRouter from "./routes/orderRouter.js";
 
 //=================app config========================
 
@@ -26,6 +27,7 @@ app.use("/api/food", foodRouter);
 app.use("/images", express.static("uploads"));
 app.use("/api/user", userRouter);
 app.use("/api/cart", cartRouter);
+app.use("/api/order", orderRouter);
 
 app.get("/", (req, res) => {
   res.send("Welcome to my API");
--- Frontend/src/Pages/PlaceOrder/PlaceOrder.jsx
+++ Frontend/src/Pages/PlaceOrder/PlaceOrder.jsx
@@ -1,27 +1,130 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import "./PlaceOrder.css";
 import { StoreContext } from "../../Context/StoreCntext";
-import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const PlaceOrder = () => {
-  const { TotalCartAmount } = useContext(StoreContext);
-  const navigate = useNavigate();
+  const { TotalCartAmount, food_list, cartItems, url, token } =
+    useContext(StoreContext);
+
+  const [data, setData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    street: "",
+    city: "",
+    province: "",
+    phone: "",
+  });
+
+  const onChangeHandler = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setData((data) => ({ ...data, [name]: value }));
+  };
+
+  const placeOrder = async (event) => {
+    event.preventDefault();
+    const orderItems = [];
+    food_list.map((item) => {
+      if (cartItems[item._id] > 0) {
+        let itemInfo = item;
+        itemInfo["quantity"] = cartItems[item._id];
+        orderItems.push(itemInfo);
+      }
+    });
+    let orderData = {
+      address: data,
+      items: orderItems,
+      amount: TotalCartAmount() + Math.round(TotalCartAmount() * 0.2),
+    };
+
+    try {
+      let response = await axios.post(url + "/api/order/place", orderData, {
+        headers: { token },
+      });
+      if (response.data.success) {
+        // Assuming the server responds with a Stripe session URL
+        window.location.replace(response.data.session_url);
+      } else {
+        // Display a more user-friendly error message
+        alert(
+          "An error occurred while placing the order. Please try again later."
+        );
+        console.error("Error placing order:", response.data.error);
+      }
+    } catch (error) {
+      // Handle any other errors that may occur
+      alert("An unexpected error occurred. Please try again later.");
+      console.error("Error placing order:", error);
+    }
+  };
+
   return (
-    <form className="placeorfer">
+    <form onSubmit={placeOrder} className="placeorfer">
       <div className="placeorder-left">
         <p className="title">Delivery Information</p>
 
         <div className="order-fields">
-          <input type="text" placeholder="First Name " />
-          <input type="text" placeholder="Second Name" />
+          <input
+            required
+            name="firstName"
+            onChange={onChangeHandler}
+            value={data.firstName}
+            type="text"
+            placeholder="First Name "
+          />
+          <input
+            required
+            name="lastName"
+            onChange={onChangeHandler}
+            value={data.lastName}
+            type="text"
+            placeholder="Second Name"
+          />
         </div>
-        <input type="email" placeholder="Email " />
-        <input type="text" placeholder="House No., Street" />
+        <input
+          required
+          name="email"
+          onChange={onChangeHandler}
+          value={data.email}
+          type="email"
+          placeholder="Email "
+        />
+        <input
+          required
+          name="street"
+          onChange={onChangeHandler}
+          value={data.street}
+          type="text"
+          placeholder="House No., Street"
+        />
         <div className="order-fields">
-          <input type="text" placeholder="City " />
-          <input type="text" placeholder="Province" />
+          <input
+            required
+            name="city"
+            onChange={onChangeHandler}
+            value={data.city}
+            type="text"
+            placeholder="City "
+          />
+          <input
+            required
+            name="province"
+            onChange={onChangeHandler}
+            value={data.province}
+            type="text"
+            placeholder="Province"
+          />
         </div>
-        <input type="text" placeholder="Phone Number" />
+        <input
+          required
+          name="phone"
+          onChange={onChangeHandler}
+          value={data.phone}
+          type="text"
+          placeholder="Phone Number"
+        />
       </div>
 
       <div className="placeorder-right">
@@ -35,15 +138,15 @@ const PlaceOrder = () => {
             <hr />
             <div className="cart-total-details">
               <p>Delivery Fee</p>
-              <p>{250}</p>
+              <p>{Math.round(TotalCartAmount() * 0.2)}</p>
             </div>
             <hr />
             <div className="cart-total-details">
               <b>Total</b>
-              <b>{TotalCartAmount() + 250}</b>
+              <b>{TotalCartAmount() + Math.round(TotalCartAmount() * 0.2)}</b>
             </div>
           </div>
-          <button onClick={() => navigate("/")}>Proceed to Payment</button>
+          <button type="submit">Proceed to Payment</button>
         </div>
       </div>
     </form>
