b46b111a1605983bb1f66e5c94d1f1bbbda7d6ec
sithum jeevantha <sithum.21@cse.mrt.ac.lk>
2024-07-06 23:09:07 +0530

    latest arrivals added

---
+++ Frontend/src/Components/LatestArrivals/HistoryArrivals.jsx
@@ -0,0 +1,35 @@
+import React, { useContext } from "react";
+import "./LatestArrivals.css";
+import { StoreContext } from "../../Context/StoreCntext";
+import FoodItem from "../FoodItem/FoodItem";
+
+const HistoryArrivals = () => {
+  const { food_list } = useContext(StoreContext);
+  const latest_list = food_list
+    .filter((item) => item.category === "History")
+    .slice(-10);
+
+  return (
+    <div className="latest-arrivals">
+      <h1>Latest History Books</h1>
+      <div className="latest-arrival-list">
+        {latest_list.map((item, index) => {
+          return (
+            <FoodItem
+              key={index}
+              id={item._id}
+              name={item.name}
+              authorName={item.authorName}
+              category={item.category}
+              price={item.price}
+              image={item.image}
+            />
+          );
+        })}
+      </div>
+      <hr />
+    </div>
+  );
+};
+
+export default HistoryArrivals;
--- Frontend/src/Pages/Home/Home.jsx
+++ Frontend/src/Pages/Home/Home.jsx
@@ -1,11 +1,21 @@
 import Header from "../../Components/Header/Header";
 import "./Home.css";
 import AppDownload from "../../Components/AppDownload/AppDownload";
+import RomanceArrivals from "../../Components/LatestArrivals/RomanceArrivals";
+import ChildrenArrivals from "../../Components/LatestArrivals/ChildrenArrivals";
+import FictionArrivals from "../../Components/LatestArrivals/FictionArrivals";
+import HistoryArrivals from "../../Components/LatestArrivals/HistoryArrivals";
+import CookbooksArrivals from "../../Components/LatestArrivals/CookbooksArrivals";
 
 const Home = () => {
   return (
     <div className="home">
       <Header />
+      <RomanceArrivals />
+      <ChildrenArrivals />
+      <FictionArrivals />
+      <HistoryArrivals />
+      <CookbooksArrivals />
       <AppDownload />
     </div>
   );
